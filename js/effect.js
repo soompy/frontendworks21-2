ecb64478b3001f7c04af587d1b0bf5136c64fb9c
soompy <sooper1137@gmail.com>
2021-09-14 02:52:49 +0900

    반응형 애니메이션 적용

--- js/effect.js
+++ js/effect.js
@@ -11,7 +11,7 @@ window.addEventListener('scroll', function() {
 
 
 const rollingWrap = document.querySelector('.rolling-box');
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 20; i++) {
     const rollingText = document.createElement('span');
     rollingWrap.appendChild(rollingText);
     rollingText.innerText = "세계인의 사랑을 받는 글로벌 기업으로 나아갑니다";
