ff19623f9a69181d6ab138031552ad7dbb109df3
smartscore_smlee <smlee@smartscore.kr>
2021-09-23 17:44:44 +0900

    반응형

--- js/effect.js
+++ js/effect.js
@@ -21,7 +21,7 @@ const rollingWrap = document.querySelector('.rolling-box');
 for (let i = 0; i < 20; i++) {
     const rollingText = document.createElement('span');
     rollingWrap.appendChild(rollingText);
-    rollingText.innerText = "세계인의 사랑을 받는 글로벌 기업으로 나아갑니다";
+    rollingText.innerText = "세계인의 사랑을 받는 글로벌 기업으로 나아갑니다!";
 }
 
 
